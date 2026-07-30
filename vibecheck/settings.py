7f396ac80d448c555ccb9f2e7117781232fdee8c
Akarshan <poudelakarshan123@gmail.com>
2026-07-30 18:53:34 +0545

    add Confidence badge

--- vibecheck/settings.py
+++ vibecheck/settings.py
@@ -154,7 +154,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # ---- Internationalisation ---------------------------------------------
 LANGUAGE_CODE = "en-us"
-TIME_ZONE     = "NPT"
+TIME_ZONE = 'Asia/Kathmandu'
 USE_I18N      = True
 USE_TZ        = True
 
